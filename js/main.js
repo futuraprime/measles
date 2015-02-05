2270d4d2f2cdd06eb3468d41c8f61ee5fd5d5bd6
Evan Hensleigh <futuraprime@gmail.com>
2015-02-04 22:48:05 -0500

    Some measeling around.

--- js/main.js
+++ js/main.js
@@ -4,22 +4,6 @@ var yellow = ['#664400','#8A6318','#AD8536','#D0A95C','#F2CE88'];
 var green = ['#1E5124','#337331','#509453','#77B479','#A8D4A1'];
 var grey = ['#353131','#5E5959','#8A8585','#B7B4B4','#E6E5E5'];
 
-terra.registerCA({
-  type: 'GoL',
-  colorFn: function () {
-    return this.alive ? this.color + ',1' : '0,0,0,0';
-  },
-  process: function (neighbors, x, y) {
-    var surrounding = neighbors.filter(function (spot) {
-      return spot.creature.alive;
-    }).length;
-    this.alive = surrounding === 3 || surrounding === 2 && this.alive;
-    return true;
-  }
-}, function () {
-  this.alive = Math.random() < 0.5;
-});
-
 
 terra.registerCA({
   type : 'infectable',
@@ -32,22 +16,39 @@ terra.registerCA({
     dead : chroma(grey[2]).rgba()
   },
   colorFn : function() {
-    // console.log(this.state, this.colors[this.state]);
     return this.colors[this.state];
   },
   process : function(neighbors, x, y) {
+    var exposed = neighbors.filter(function(i) {
+      return i.creature.state === 'sick';
+    });
+    if( exposed.length > 0 &&
+      ['healthy', 'vaccinatedFail'].indexOf(this.state) > -1 &&
+      Math.random() > Math.pow(0.8, exposed.length)
+    ) {
+      this.state = 'sick';
+      return true;
+    }
     return true;
   }
 }, function() {
-  this.state = 'healthy';
+  var rand = Math.random();
+  this.state = rand > 0.9 ? 'immune' : 'healthy';
+  this.state = rand < 0.001 ? 'sick' : this.state;
+  if(this.state === 'healthy') {
+    var rand2 = Math.random();
+    this.state = rand2 > 0.9 ? 'healthy' : 'vaccinated'
+    this.state = rand2 < 0.1 ? 'vaccinatedFail' : this.state;
+  }
 });
 
-var terrarium = new terra.Terrarium(25, 25, {
+var terrarium = new terra.Terrarium(100, 100, {
   id : 'terrarium',
+  cellSize : 4,
   background : chroma(grey[3]).rgb(),
   insertAfter : document.getElementById('fieldmarker')
 });
 
 terrarium.grid = terrarium.makeGrid('infectable');
 
-terrarium.animate(5);
+terrarium.animate(300);
